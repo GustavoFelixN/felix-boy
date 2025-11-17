8979312fe395d92d4ae0587d7305b4e6b668fbc8
Gustavo Felix <gustavo.felixdn@gmail.com>
2025-11-17 16:46:42 -0300

    adding returns

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -91,47 +91,48 @@ void CPU::execute(uint8_t opcode) {
     if(( opcode & 0b11000000 ) == 0b01000000) {
         uint8_t dest = (opcode >> 3) & 0b111;
         uint8_t src = opcode & 0b111;
-        LD_r_r(static_cast<Reg8>(dest), static_cast<Reg8>(src));
+        return LD_r_r(static_cast<Reg8>(dest), static_cast<Reg8>(src));
     }
 
     else if((opcode & 0b11000111) == 0b00000110) {
         uint8_t dest = (opcode >> 3) & 0b111;
-        LD_r_n(static_cast<Reg8>(dest));
+        return LD_r_n(static_cast<Reg8>(dest));
     }
 
-    else if(opcode == 0b00001010) { LD_a_mem(REG_BC); }
-    else if(opcode == 0b00011010) { LD_a_mem(REG_DE); }
-    else if(opcode == 0b00000010) { LD_mem_a(REG_BC); }
-    else if(opcode == 0b00010010) { LD_mem_a(REG_DE); }
-    else if(opcode == 0b11111010) { LD_a_nn(); }
-    else if(opcode == 0b11101010) { LD_nn_a(); }
-    else if(opcode == 0b11110010) { LDH_a_c(); }
-    else if(opcode == 0b11100010) { LDH_c_a(); }
-    else if(opcode == 0b11110000) { LDH_a_n(); }
-    else if(opcode == 0b11100000) { LDH_n_a(); }
+    else if(opcode == 0b00001010) { return LD_a_mem(REG_BC); }
+    else if(opcode == 0b00011010) { return LD_a_mem(REG_DE); }
+    else if(opcode == 0b00000010) { return LD_mem_a(REG_BC); }
+    else if(opcode == 0b00010010) { return LD_mem_a(REG_DE); }
+    else if(opcode == 0b11111010) { return LD_a_nn(); }
+    else if(opcode == 0b11101010) { return LD_nn_a(); }
+    else if(opcode == 0b11110010) { return LDH_a_c(); }
+    else if(opcode == 0b11100010) { return LDH_c_a(); }
+    else if(opcode == 0b11110000) { return LDH_a_n(); }
+    else if(opcode == 0b11100000) { return LDH_n_a(); }
 
-    else if(opcode == 0b00111010) { LD_a_hl_decrement(); }
-    else if(opcode == 0b00110010) { LD_hl_a_decrement(); }
-    else if(opcode == 0b00101010) { LD_a_hl_increment(); }
-    else if(opcode == 0b00100010) { LD_hl_a_increment(); }
+    else if(opcode == 0b00111010) { return LD_a_hl_decrement(); }
+    else if(opcode == 0b00110010) { return LD_hl_a_decrement(); }
+    else if(opcode == 0b00101010) { return LD_a_hl_increment(); }
+    else if(opcode == 0b00100010) { return LD_hl_a_increment(); }
 
     else if(( opcode  & 0b11001111) == 0b00000001) {
         uint8_t dest = (opcode >> 4) & 0x03;
-        LD_rr_nn(static_cast<Reg16>(dest));
+        return LD_rr_nn(static_cast<Reg16>(dest));
     }
-    else if(opcode == 0b00001000) { LD_nn_sp(); }
-    else if(opcode == 0b11111001) { LD_sp_hl(); }
+    else if(opcode == 0b00001000) { return LD_nn_sp(); }
+    else if(opcode == 0b11111001) { return LD_sp_hl(); }
 
     else if((opcode & 0b11001111) == 0b11000101) {
         uint8_t src  = (opcode >> 4) & 0x03;
-        PUSH(static_cast<Reg16>(src));
-    }
-    else if((opcode & 0b11001111) == 0b11000001) {
-        uint8_t dest  = (opcode >> 4) & 0x03;
-        POP(static_cast<Reg16>(src));
+        return PUSH(static_cast<Reg16>(src));
     }
 
-    else if(opcode == 0b11111000) { LD_hl_sp_e() }
+    else if((opcode & 0b11001111) == 0b11000001) {
+        uint8_t dest  = (opcode >> 4) & 0x03;
+        return POP(static_cast<Reg16>(dest));
+    }
+
+    else if(opcode == 0b11111000) { return LD_hl_sp_e(); }
 }
 void CPU::executeNext() {
     uint8_t opcode = fetch();
