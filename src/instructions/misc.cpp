ec4003834e5bc214a57e850b249822d8a993b2a6
Gustavo Felix <gustavo.felixdn@gmail.com>
2025-11-19 16:32:22 -0300

    supressing warnig misc

--- src/instructions/misc.cpp
+++ src/instructions/misc.cpp
@@ -2,10 +2,10 @@
 #include "../cpu.h"
 
 namespace instr {
-    void NOP(CPU& cpu, uint8_t opcode) { cpu.NOP(); }
-    void STOP(CPU& cpu, uint8_t opcode) { cpu.STOP(); }
-    void HALT(CPU& cpu, uint8_t opcode) { cpu.HALT(); }
-    void PREFIX(CPU& cpu, uint8_t opcode) { cpu.PREFIX(); }
-    void DI(CPU& cpu, uint8_t opcode) { cpu.DI(); }
-    void EI(CPU& cpu, uint8_t opcode) { cpu.EI(); }
+    void NOP(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.NOP(); }
+    void STOP(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.STOP(); }
+    void HALT(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.HALT(); }
+    void PREFIX(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.PREFIX(); }
+    void DI(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.DI(); }
+    void EI(CPU& cpu, uint8_t opcode) { (void) opcode; cpu.EI(); }
 }
